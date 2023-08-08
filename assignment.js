4417c544b880f98b8c9140d03d46113c69cbc015
freelancerjoynal <developerjoynal@gmail.com>
2023-08-09 00:57:04 +0600

    second problem solved

--- assignment.js
+++ assignment.js
@@ -4,8 +4,8 @@ function cubeNumber(number) {
     var type = typeof (input);
     if (type == 'number') {
         if (input > 0) {
-            var result = number ** 3;
-            return result;
+            var qube = number ** 3;
+            return qube;
         } else {
             var result = 'Only positive numbers or bigger than 0 is allowed';
             return result;
@@ -19,5 +19,23 @@ function cubeNumber(number) {
 
 
 // Proble two
+function matchFinder(string1, string2) {
+    if (typeof (string1) == 'string' && typeof (string2) == 'string') {
+
+        if(string1.match(string2)){
+            return true;
+        }else{
+            return false;
+        }
+
+
+    } else {
+        return 'Only texts are allowed, please input a valid data';
+    }
+
+}
+
+// result = matchFinder('John Doe', 'Doe');
+// console.log(result);
 
 
