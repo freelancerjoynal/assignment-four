3a4e8060a6d2641d5ac268cdc9fd22d9e09d6b3a
freelancerjoynal <developerjoynal@gmail.com>
2023-08-09 00:45:11 +0600

    problem one solved

--- assignment.js
+++ assignment.js
@@ -1,1 +1,23 @@
-console.log('Login in');
+// Problem One
+function cubeNumber(number) {
+    var input = number;
+    var type = typeof (input);
+    if (type == 'number') {
+        if (input > 0) {
+            var result = number ** 3;
+            return result;
+        } else {
+            var result = 'Only positive numbers or bigger than 0 is allowed';
+            return result;
+        }
+
+    } else {
+        var result = 'Only numbers allowed, please input a valid data';
+        return result;
+    }
+}
+
+
+// Proble two
+
+
